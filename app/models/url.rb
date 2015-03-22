1ae129572acae7ddbaa8fbec522fde0365a84d79
Alf Mikula <amikula@gmail.com>
2015-03-22 12:14:25 -0700

    Add url validation

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,6 +1,8 @@
 class Url < ActiveRecord::Base
   SLUG_CHARS = ('A'..'Z').to_a + ('a'..'z').to_a + (0..9).to_a
 
+  validates_format_of :url, :with => URI::regexp(%w(http https))
+
   before_save do
     unless self.slug.present?
       self.slug = (1..7).collect{SLUG_CHARS[rand(SLUG_CHARS.length)]}.join("")
--- spec/models/url_spec.rb
+++ spec/models/url_spec.rb
@@ -1,10 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Url, :type => :model do
-  it 'creates a 7-character alphanumeric slug automatically if one is not assigned upon create' do
-    url = Url.create(url: "http://www.example.com/path")
-    expect(url.slug).to_not be(nil)
-    expect(url.slug.length).to eq(7)
-    expect(url.slug).to match(/^[A-Za-z0-9]{7}$/)
+  %w{http https}.each do |prefix|
+    it "creates a 7-character alphanumeric slug automatically if one is not assigned upon create for #{prefix} urls" do
+      url = Url.create(url: "#{prefix}://www.example.com/path")
+      expect(url.slug).to_not be(nil)
+      expect(url.slug.length).to eq(7)
+      expect(url.slug).to match(/^[A-Za-z0-9]{7}$/)
+    end
+  end
+
+  it 'rejects urls that do not start with http:// or https://' do
+    expect {
+      Url.create!(url: "www.example.com")
+    }.to raise_error(ActiveRecord::RecordInvalid)
   end
 end
